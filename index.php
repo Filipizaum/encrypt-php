1fea358cf43752277cb466ec63e3886f7cedbb58
Felipe Lima <filipizaum@gmail.com>
2018-05-23 00:34:01 -0400

    WIP Ajustes para php7.0
    Signed-off-by: Felipe Lima <filipizaum@gmail.com>

--- index.php
+++ index.php
@@ -1,6 +1,42 @@
 <!DOCTYPE html>
+
+<?php
+
+function pad_key($key){
+    // key is too large
+    if(strlen($key) > 32) return false;
+
+    // set sizes
+    $sizes = array(16,24,32);
+
+    // loop until string length matches a valid size
+    while(!in_array(strlen($key), $sizes)){
+        $key = $key."\0";
+    }
+
+    // return
+    return $key;
+}
+
+if(isset($_POST['encrypt']))
+    $encrypt = $_POST['encrypt'];
+if(isset($_POST['decrypt']))
+    $decrypt = $_POST['decrypt'];
+
+if(isset($_POST['pass'])){
+    error_log("tamanho da string");
+    $pass = $_POST['pass'];
+    error_log(strlen($pass));
+    $pass = pad_key($_POST['pass']);
+    error_log(strlen($pass));
+}
+
+
+?>
+
 <html>
 <head>
+        <meta charset="ISO-8859-1"> 
 	<title>Encrypt</title>
 </head>
 <body>
@@ -38,7 +74,7 @@
 			<td>
 				<?php
 					if(isset($_POST['encrypt'])){
-						$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $_POST['pass'], $_POST['encrypt'], MCRYPT_MODE_CBC);
+						$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $pass, $_POST['encrypt'], MCRYPT_MODE_CBC);
 						$encrypted64 = base64_encode($encrypted);
 						echo "<br/><span>".$encrypted64."</span>";
 					}
@@ -48,7 +84,11 @@
 				<?php
 					if(isset($_POST['decrypt'])){
 						$encrypted = base64_decode($_POST['decrypt']);
-						$result = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $_POST['pass'], $encrypted, MCRYPT_MODE_CBC);
+                                                $ivSize = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+                                                error_log('Tamanho o vetor: '. $ivSize);
+                                                $iv = substr($encrypted, 0, $ivSize);
+                                                error_log('Tamanho a string: '. strlen($iv));
+						$result = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $pass, $encrypted, MCRYPT_MODE_CBC, $iv);
 						echo "<br/><span>".$result."</span>";
 					}
 				?>
